a4bbea2caf612da6c7b0a54897ee0523a099298c
Pace Zhong <pace_zhong@foxmail.com>
2021-08-05 17:41:38 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+
+/**
+ * 日志上传方法, 合并一定时长内的日志为数组
+ * @config {
+ *  duration: int, 非必填, 合并时长
+ *  upload: Fn, 非必填自定义上传方法
+ * }
+ * 
+ * 示例: 
+ * const logger = new Logger();
+ * logger.send('xxxx');
+ */
+
+class Logger {
+
+    constructor({
+        duration=100,
+        upload
+    }) {
+        this.duration = duration; 
+        this.logQueue = [];
+        this.timer = null;
+        upload && (this.upload = upload)
+    }
+
+    send(string) {
+
+        // 添加
+        this.logQueue.push(string)
+        
+        if(!this.timer) {
+            this.timer = setTimeout(() => {
+                this.upload(this.logQueue); // 执行日志
+                this.reset(); // 重置状态
+            },this.duration)
+        }
+    }
+
+    upload(logQueue) {
+        // 执行上传
+        console.log('excute default upload function, logQueue:', logQueue)
+    }
+
+    reset() {
+        // 执行后重置内部状态
+        this.logQueue = []; 
+        this.timer = null;  
+    }
+}
+
+logger = new UploadLogger({
+    duration: 1000,
+    upload:  (logQueue) => {
+        console.log('logQueue:',logQueue)
+    }
+});
